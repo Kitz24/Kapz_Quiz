cb98e308219f911cd333674405053c7c19d0a284
Kittu <krishnaroop0710@gmail.com>
2023-02-04 17:11:16 +0530

    Beta v0.1 fully done

---
+++ app/src/main/java/com/example/kapzquiz/TopicSelectActivity.java
@@ -0,0 +1,21 @@
+package com.example.kapzquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class TopicSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic_select);
+    }
+
+    public void onClickbook(View view){
+        Intent intent = new Intent(TopicSelectActivity.this,QuestionPage.class);
+        startActivity(intent);
+    }
+}
